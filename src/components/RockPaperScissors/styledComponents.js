b2911bdfc21772d7f5de80fe5d68925a1e99e242
ramya <ramyapotikalapudi@gmail.com>
2024-04-21 13:41:52 +0000

    solution

---
+++ src/components/RockPaperScissors/styledComponents.js
@@ -0,0 +1,86 @@
+import styled from 'styled-components'
+
+export const AppContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+`
+export const ResultContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+`
+export const OptionsContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+`
+export const Option = styled.h1`
+    font-size: 16px;
+    font-family: 'Roboto';
+`
+export const ScoreContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+`
+export const ScorePhase = styled.p`
+    font-size: 16px;
+    font-family: 'Roboto';
+`
+export const ScoreNumber = styled.p`
+    font-size: 16px;
+    font-family: 'Roboto';
+`
+export const GameViewContainer = styled.div`
+    display: flex;
+    justify-content: center;
+`
+export const GameOptionsList = styled.ul`
+    display: flex;
+    flex-direction: row;
+`
+export const PopupContainer = styled.div`
+    display: flex;
+    justify-content: center;
+`
+
+export const TriggerButton = styled.button`
+    font-size: 16px;
+    font-family: 'Roboto';
+`
+export const CloseButton = styled.button`
+    font-size: 16px;
+    font-family: 'Roboto';
+`
+export const PopupImage = styled.img`
+    width:90%;
+`
+export const PopupBody = styled.div`
+    display: flex;
+    justify-content: space-between;
+`
+export const GameResultViewContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+`
+export const SelectedOptionsContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+`
+export const GameUserOptionContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+`
+export const GameParticipantText = styled.p`
+    font-size: 16px;
+    font-family: 'Roboto';
+`
+export const GameParticipantChoiceImage = styled.img`
+    width: 200px;
+    height: 200px;
+`
+export const ResultText = styled.p`
+    font-size: 16px;
+    font-family: 'Roboto'
+`
+export const PlayAgainBtn = styled.button`
+    font-size: 16px;
+    font-family: 'Roboto'
+`
